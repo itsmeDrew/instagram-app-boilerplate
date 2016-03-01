5a5ad801a08fba6961b2abd688cfa3685bf8d7e7
Drew Roberts <drewr@admins-MacBook-Pro-5.local>
2016-03-01 10:43:26 -0500

    removed unused dependancies

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,16 +1,11 @@
 'use strict';
 
-var $ = require('jquery-browserify');
-
 require('angular');
-require('angularfire');
-require('angular-ui-router');
 require('./templates');
 
 require('./services/ig');
 
 var app = angular.module('App', [
-  'ui.router',
   'templates',
   'App.Service.Instagram'
 ]);
@@ -18,5 +13,3 @@ var app = angular.module('App', [
 app.controller('AppCtrl', require('./controllers/AppCtrl'));
 
 app.constant('App.Config', window.config);
-
-app.config(require('./on_config'));
